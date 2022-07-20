62079bc23386fdc925c29cb26434c7877898d818
Paooidk <103084858+Paooidk@users.noreply.github.com>
2022-07-20 17:02:49 -0400

    Eliminar niveles 2,3,4.gd

--- escenas/levels/Nivel2.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-onready var bullet_manager=$BulletManager
-onready var player=$Player
-
-func _ready():
-	randomize()
-	SenalesGlobales.connect('disparo',bullet_manager,'handle_bullet_spawned')
